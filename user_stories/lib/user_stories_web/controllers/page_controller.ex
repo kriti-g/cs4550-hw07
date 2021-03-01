c7270967d7cbb1d850a8d1b55a033cc572653478
kriti-g <kriti7000@gmail.com>
2021-02-28 19:35:52 -0500

    controller update

--- user_stories/lib/user_stories_web/controllers/page_controller.ex
+++ user_stories/lib/user_stories_web/controllers/page_controller.ex
@@ -1,7 +1,11 @@
 defmodule UserStoriesWeb.PageController do
   use UserStoriesWeb, :controller
 
+  alias UserStories.Events
+  alias UserStories.Events.Event
+
   def index(conn, _params) do
-    render(conn, "index.html")
+    events = Events.list_events()
+    render(conn, "index.html", events: events)
   end
 end
