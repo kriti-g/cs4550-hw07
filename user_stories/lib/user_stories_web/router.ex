9ae7bb687602a22032ad213f95bd20b042153e67
kriti-g <kriti7000@gmail.com>
2021-03-03 18:47:51 -0500

    User session contexts

--- user_stories/lib/user_stories_web/router.ex
+++ user_stories/lib/user_stories_web/router.ex
@@ -7,6 +7,7 @@ defmodule UserStoriesWeb.Router do
     plug :fetch_flash
     plug :protect_from_forgery
     plug :put_secure_browser_headers
+    plug UserStoriesWeb.Plugs.FetchSession
   end
 
   pipeline :api do
