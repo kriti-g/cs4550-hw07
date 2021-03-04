8b3c1e5851277e57f20d1f8f63d86155d1bac47c
kriti-g <kriti7000@gmail.com>
2021-03-03 19:07:34 -0500

    update event

--- user_stories/priv/repo/migrations/20210228235725_create_events.exs
+++ user_stories/priv/repo/migrations/20210228235725_create_events.exs
@@ -4,7 +4,6 @@ defmodule UserStories.Repo.Migrations.CreateEvents do
   def change do
     create table(:events) do
       add :name, :string, null: false
-      add :user, :string, null: false
       add :date, :naive_datetime, null: false
       add :desc, :text, null: false
 
