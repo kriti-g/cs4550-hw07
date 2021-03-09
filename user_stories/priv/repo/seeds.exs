07949d6a110158712d6a390386e1057ada4a909a
kriti-g <kriti7000@gmail.com>
2021-03-08 21:21:58 -0500

    Update seeds.exs

--- user_stories/priv/repo/seeds.exs
+++ user_stories/priv/repo/seeds.exs
@@ -9,3 +9,13 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+alias UserStories.Repo
+alias UserStories.Users.User
+alias UserStories.Events.Event
+
+alice = Repo.insert!(%User{name: "alice", email: "alice@email.com"})
+bob = Repo.insert!(%User{name: "bob", email: "bob@email.com"})
+
+Repo.insert!(%Event{user_id: alice.id, name: "Alice Post", desc: "Alice says Hi!", date: ~N[2001-01-01 23:00:00]})
+Repo.insert!(%Event{user_id: bob.id, name: "Bob Post", desc: "Bob says garblarg!", date: ~N[2004-01-19 23:00:00]})
